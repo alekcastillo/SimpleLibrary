eec9631d5618a5f215918ea3b2db0a05dfa269e0
Adrianvc94 <63268239+Adrianvc94@users.noreply.github.com>
2020-07-06 20:19:40 -0600

    Add files via upload

---
+++ Models/Proyecto_Clases/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Clases
+{
+    public class Book
+    {
+        Guid Id;
+        BookType Type;
+        BookStatus Status;
+
+        public static void Add() { }
+
+        public void Lend(Guid userId) { }
+
+        public void Lend(string userEmail) { }
+
+        public void SetAsLost() { }
+
+        public void Return() { }
+    }
+}
---
+++ Models/Proyecto_Clases/BookType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Clases
+{
+    public class BookType
+    {
+        Guid Id;
+        string Title;
+        BookSubject Subject;
+        BookAuthor Author;
+        BookPublisher Publisher;
+        int Year;
+        int Pages;
+
+        public static void Add() { }
+    }
+}
